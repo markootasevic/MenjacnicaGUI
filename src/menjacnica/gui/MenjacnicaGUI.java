9ae5c96b05b730b3c5d5ee42ab3b75334acb0c4c
markootasevic <motasevic994@gmail.com>
2015-04-26 21:59:40 +0200

    Dodate funkcije opcijama open,save,about,exit iz navigacionog menija

--- src/menjacnica/gui/MenjacnicaGUI.java
+++ src/menjacnica/gui/MenjacnicaGUI.java
@@ -6,30 +6,48 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.Toolkit;
+
+import javax.swing.JFileChooser;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextArea;
+
 import java.awt.FlowLayout;
+
 import javax.swing.JPopupMenu;
+
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import javax.swing.border.TitledBorder;
 import javax.swing.BoxLayout;
+
 import java.awt.GridLayout;
 import java.awt.CardLayout;
+
 import net.miginfocom.swing.MigLayout;
+
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MenjacnicaGUI extends JFrame {
 
@@ -44,7 +62,7 @@ public class MenjacnicaGUI extends JFrame {
 	private JScrollPane scrollPane;
 	private JTable table;
 	private JPanel panel_2;
-	private JTextArea textArea;
+	private JTextArea textAreaIspis;
 	private JPanel panel_1;
 	private JButton btnDodajKurs;
 	private JButton btnObrisiKurs;
@@ -53,6 +71,7 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenuItem mntmDodajKurs;
 	private JMenuItem mntmObrisiKurs;
 	private JMenuItem mntmIzvrsiZamenu;
+	private JMenuItem mntmAbout;
 
 	/**
 	 * Launch the application.
@@ -74,8 +93,15 @@ public class MenjacnicaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenjacnicaGUI() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				ugasiAplikaciju();
+			}
+			
+		});
 		setTitle("Menjacnica");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 618, 419);
 		setJMenuBar(getMenuBar_1());
 		contentPane = new JPanel();
@@ -105,12 +131,26 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenu getMnHelp() {
 		if (mnHelp == null) {
 			mnHelp = new JMenu("Help");
+			mnHelp.add(getMntmAbout());
 		}
 		return mnHelp;
 	}
 	private JMenuItem getMntmOpen() {
 		if (mntmOpen == null) {
 			mntmOpen = new JMenuItem("Open");
+			mntmOpen.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JFileChooser jfc = new JFileChooser();
+					int returnVal = jfc.showOpenDialog(contentPane);
+					
+					if (returnVal == jfc.APPROVE_OPTION) {
+						File fajl = jfc.getSelectedFile();
+						String a = textAreaIspis.getText();
+						textAreaIspis.setText(a + "\n" + "Ucitan fajl:" + fajl.getAbsolutePath());
+						
+					}
+				}
+			});
 			mntmOpen.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/com/sun/java/swing/plaf/windows/icons/Directory.gif")));
 			mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
 		}
@@ -119,6 +159,19 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenuItem getMntmSave() {
 		if (mntmSave == null) {
 			mntmSave = new JMenuItem("Save");
+			mntmSave.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JFileChooser jfc = new JFileChooser();
+					int returnVal = jfc.showSaveDialog(contentPane);
+					
+					if (returnVal == jfc.APPROVE_OPTION) {
+						File fajl = jfc.getSelectedFile();
+						String a = textAreaIspis.getText();
+						textAreaIspis.setText(a + "\n" + "Sacuvan fajl:" + fajl.getAbsolutePath());
+
+					
+				}
+				}});
 			mntmSave.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/com/sun/java/swing/plaf/windows/icons/FloppyDrive.gif")));
 			mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		}
@@ -127,6 +180,11 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenuItem getMntmExit() {
 		if (mntmExit == null) {
 			mntmExit = new JMenuItem("Exit");
+			mntmExit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					ugasiAplikaciju();
+				}
+			});
 			mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.ALT_MASK));
 		}
 		return mntmExit;
@@ -179,15 +237,15 @@ public class MenjacnicaGUI extends JFrame {
 			panel_2 = new JPanel();
 			panel_2.setBorder(new TitledBorder(null, "Status", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			panel_2.setLayout(new BorderLayout(0, 0));
-			panel_2.add(getTextArea(), BorderLayout.NORTH);
+			panel_2.add(getTextAreaIspis(), BorderLayout.NORTH);
 		}
 		return panel_2;
 	}
-	private JTextArea getTextArea() {
-		if (textArea == null) {
-			textArea = new JTextArea();
+	private JTextArea getTextAreaIspis() {
+		if (textAreaIspis == null) {
+			textAreaIspis = new JTextArea();
 		}
-		return textArea;
+		return textAreaIspis;
 	}
 	private JPanel getPanel_1() {
 		if (panel_1 == null) {
@@ -244,5 +302,26 @@ public class MenjacnicaGUI extends JFrame {
 			mntmIzvrsiZamenu = new JMenuItem("Izvrsi zamenu");
 		}
 		return mntmIzvrsiZamenu;
+	}
+	private void ugasiAplikaciju() {
+		int opcija = JOptionPane.showConfirmDialog(contentPane,
+				"da li zaista zelite da izadjete iz aplikacije", "izlazak",
+				JOptionPane.YES_NO_CANCEL_OPTION);
+		if (opcija == JOptionPane.YES_OPTION)
+			System.exit(0);
+	}
+	private JMenuItem getMntmAbout() {
+		if (mntmAbout == null) {
+			mntmAbout = new JMenuItem("About");
+			mntmAbout.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JOptionPane.showMessageDialog(contentPane,
+							"Autor: Marko, verzija 1.0 "
+							+ "\n" + "Ovo je jedan programcic", "O programu: ",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+			});
+		}
+		return mntmAbout;
 	}
 }
